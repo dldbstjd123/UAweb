4162306677e01c28fffde07f478f7e749bbac655
Yoon Jung <dannydannyl@me.com>
2020-08-29 00:34:18 -0400

    npm build ready for production, listening on port 3001

--- api/app.js
+++ api/app.js
@@ -5,8 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
 var emailRouter = require('./routes/email');
 
 var app = express();
@@ -21,10 +21,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname + '/../client/build/')));
 
-app.use('/', indexRouter);
+// app.use('/', indexRouter);
 app.use('/email', emailRouter);
-app.use('/users', usersRouter);
+// app.use('/users', usersRouter);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+ '/../client/build/index.html'))
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ client/build/precache-manifest.a56dcef5e4a19eb80c19ab035848bd34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ba71103c43ce45dcd12f8a4d38031c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b72906b8af0ef4f84c03",
+    "url": "/static/css/main.7ebda62c.chunk.css"
+  },
+  {
+    "revision": "734dc0475fd96a773abe",
+    "url": "/static/js/2.11808690.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.11808690.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b72906b8af0ef4f84c03",
+    "url": "/static/js/main.824510e9.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
